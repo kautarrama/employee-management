bce36f493991f10fc9b7f769123118ff46bb07da
Khautal Hilmi Ramadhan <khautalrama@gmail.com>
2023-12-08 22:08:55 +0700

    feat: enhance layout header and footer, add iconify

---
+++ app/Views/layout/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Karyawan</title>
+    <link rel="stylesheet" href="<?= base_url() ?>/css/styles.css?v=1.0">
+    <script src="//code.iconify.design/1/1.0.6/iconify.min.js"></script>
+</head>
+<body>
+    
