3bf4c662db918756284dcf786866667f737f1808
Khautal Hilmi Ramadhan <khautalrama@gmail.com>
2023-12-09 12:00:34 +0700

    feat: remove welcome route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/', 'Home::index');
 $routes->get('/', 'Employees::index');
 $routes->get('/new', 'Employees::new');
 $routes->post('/create', 'Employees::create');
